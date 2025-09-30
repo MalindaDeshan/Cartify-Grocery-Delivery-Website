2ba56e481fe5bcb1bf99d1590ec8239bbe13ad2d
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-09-30 13:40:19 -0700

    create,placed and fix  the NewsLetter

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -13,14 +13,14 @@ const BottomBanner = () => {
       md:justify-center pt-16 md:pt-0 md:pr-24'>
 
         <div>
-            <h1 className='text-2xl md:text-3xl font-semibold text-primary mb-6'>
+            <h1 className='text-2xl md:text-3xl font-semibold text-primary-text mb-6'>
                 Why We Are the Best?</h1>
             {features.map((feature,index) => (
                 <div key={index} className='flex items-center gap-4 mt-2'>
                     <img src={feature.icon} alt={feature.title} className='md:w-11 w-9' />
                     <div>
                         <h3 className='text-lg md:text-xl font-semibold'>{feature.title}</h3>
-                    <p className='text-gray-500 text-xs md:text-sm'>{feature.description}</p>
+                    <p className='text-xs md:text-sm'>{feature.description}</p>
                     </div>
                 </div>
                     
