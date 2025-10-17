24bee0cbd4755dfbe7c55dfa31f79c6c92759b69
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-10-17 03:06:29 -0700

    Seller-Layout page create and mount

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -25,13 +25,15 @@ const SellerLogin = () => {
             <p className='text-2xl font-medium m-auto'><span className='text-primary'>Seller</span>Login</p>
             <div className='w-full'>
                 <p>Email</p>
-                <input type="email" placeholder='enter your email'
+                <input onChange={(e) => setEmail(e.target.value)} value={email}
+                 type="email" placeholder='enter your email'
                 className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary'
                 required/>
             </div>
             <div className='w-full'>
                 <p>Password</p>
-                <input type="password" placeholder='enter your password'
+                <input onChange={(e) => setPassword(e.target.value)} value={password}
+                type="password" placeholder='enter your password'
                 className='border border-gray-200 rounded w-full p-2 mt-1 outline-primary'
                 required/>
             </div>
