e772c8631f722b352398f0ffad82251e4fb1ac97
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-10-17 00:19:49 -0700

    Create SellerLogin interface and mount it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,13 +12,14 @@ import DetailsProduct from "./pages/DetailsProduct";
 import Cart from "./pages/Cart";
 import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
 
 
 
 const App = () => {
   const location = useLocation();
   const isSellerPath = location.pathname.includes("/seller");
-  const { showUserLogin } = useAppContext();
+  const { showUserLogin,isSeller } = useAppContext();
 
   return (
     <div>
@@ -35,6 +36,7 @@ const App = () => {
           <Route path="/cart" element={<Cart/>} />
           <Route path="/add-address" element={<AddAddress/>} />
           <Route path="/my-orders" element={<MyOrders/>} />
+          <Route path="/seller" element={isSeller ? null : <SellerLogin/>} />
         </Routes>
       </div>
 
