315d77cfb20fc8ca12cc889b5d2992f00a1a2574
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-10-25 00:20:49 -0700

    Created product schema, setup cludenery to store product images and add product  controller function

---
+++ server/controllers/productController.js
@@ -0,0 +1,40 @@
+import {v2 as cloudinary} from 'cloudinary';
+import Product from '../models/Product';
+
+//Add Product: /api/product/add
+export const addProduct = async(req, res) => {
+    try {
+        let productData = JSON.parse(req.body.productData);
+
+        const images = req.files;
+
+        let imagesUrl = await Promise.all(images.map(async(item) => {
+            let result = await cloudinary.uploader.upload(item.path,{resource_type: "image"});
+            return result.secure_url;
+        })
+    )
+
+    await Product.create({...productData, images: imagesUrl})
+
+    res.json({success: true, message: "Product added successfully"})
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({success: false, message: "Internal Server Error"})
+    }
+}
+
+//Get Product: /api/product/list
+export const productList = async(req, res) => {
+
+}
+
+//Get single Product: /api/product/id
+export const productById = async(req, res) => {
+
+}
+
+//Change Product inStock: /api/product/stock
+export const changeStock = async(req, res) => {
+
+}
