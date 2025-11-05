42afca0462daac3ef9bbb16684caf2afff526218
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-11-05 03:04:18 -0800

    port chane

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 await connectDB()
 await connectCloudinary()
